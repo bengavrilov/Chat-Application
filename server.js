69196860c765666e3dd1bbbd9c2aa7ff199871c8
Ben Gavrilov <bengavrilov@gmail.com>
2020-05-11 22:29:12 -0400

    made changes according to documentation

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 
 var express = require('express');
 var socket = require('socket.io');
+var http = require('http');
+
+const PORT = process.env.PORT || 5000;
 
 var app = express();
-
-var server = app.listen(3000, function() {
-    console.log('listening to requests on port number 3000');
-})
+var server = http.createServer(app);
 
 //const io = require('socket.io')(3000);
 const io = socket(server);
+
+server.listen(PORT, () => console.log('Server has started on port'));
 
 io.on('connection', socket => {
     socket.on('client-send-message', array => {
