2c3fb986681d8a414de755c0d0a37119291952af
Ben Gavrilov <bengavrilov@gmail.com>
2020-05-11 22:04:45 -0400

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const io = require('socket.io')(3000);
+
+var express = require('express');
+var socket = require('socket.io');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var server = app.listen(3000, function() {
+    console.log('listening to requests on port number 3000');
+})
+
+//const io = require('socket.io')(3000);
+const io = socket(server);
 
 io.on('connection', socket => {
     socket.on('client-send-message', array => {
